7a2a575db16bc5595248e1e8c93c0177864e418d
deeasuquo@gmail.com <deeasuquo@gmail.com>
2018-08-13 18:33:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments){
+  var list=[]
+  for(var i = 0; i < musicians.length;i++) {
+    var string=musicians[i]  +  ' plays '  +  instruments[i];
+      list.push(string);
+  }
+  return list
+}
+function johnLennonFacts(facts){
+  var i=0
+  while(i<facts.length){
+    facts[i] = facts[i] + "!!!" 
+    i++
+  }
+  return facts
+}
+function iLoveTheBeatles(number){
+  var list=[]
+  var i = number 
+  do{
+    var string = "I love the Beatles!"
+    list.push(string);
+    i++
+  }
+  while (i<15)
+  return list
+}
